afa031148b969bb8e1b27e23edab56c2f14a46e5
a354794@gmail.com <a354794@gmail.com>
2021-02-24 20:48:05 +0800

    add

--- app.js
+++ app.js
@@ -3,12 +3,14 @@
             osm = L.tileLayer(osmUrl, { maxZoom: 18, attribution: osmAttrib }),
             map = new L.Map('map', { center: new L.LatLng(51.505, -0.04), zoom: 13 }),
             drawnItems = L.featureGroup().addTo(map);
+
     L.control.layers({
         'osm': osm.addTo(map),
         "google": L.tileLayer('http://www.google.cn/maps/vt?lyrs=s@189&gl=cn&x={x}&y={y}&z={z}', {
             attribution: 'google'
         })
     }, { 'drawlayer': drawnItems }, { position: 'topleft', collapsed: false }).addTo(map);
+    
     map.addControl(new L.Control.Draw({
         edit: {
             featureGroup: drawnItems,
@@ -38,17 +40,43 @@
         addPopup(layer);
     });
 
-    function addPopup(layer) {
-        var content = document.createElement("textarea");
-        content.addEventListener("keyup", function () {
-            layer.feature.properties.Name = content.value;
+    function addPopup(layer){
+        let popupContent = 
+        '<form>' + 
+        'Description:<br><input type="text" id="input_desc"><br>' +
+        'Name:<br><input type="text" id="input_cena"><br>' +
+        '</form>';
+        
+        layer.bindPopup(popupContent).openPopup();
+        
+        layer.on("popupopen", function (e) {
+          var _layer = e.popup._source;
+          if(!_layer.feature){
+              _layer.feature = {
+              properties: {}
+            };
+          }
+          document.getElementById("input_desc").value = _layer.feature.properties.link || "";
+          document.getElementById("input_cena").value = _layer.feature.properties.cena || "";
+          document.getElementById("input_desc").focus();
+          openLayer = _layer;
         });
-        layer.on("popupopen", function () {
-            content.value = layer.feature.properties.Name;
-          content.focus();
-        });
-        layer.bindPopup(content).openPopup();
-    }
+        
+        layer.on("popupclose", function (e) {
+          openLayer = undefined;
+        })
+        
+      };
+      
+      L.DomEvent.on(document,"keyup",function(){
+        if(openLayer){
+          link = document.getElementById("input_desc").value;
+          cena = document.getElementById("input_cena").value;
+      
+          openLayer.feature.properties.link = link;
+          openLayer.feature.properties.cena = cena;   
+        }
+      })
     
 
     //delete  and download feature
